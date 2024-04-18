b10b890012f0521ce4abfd30414cc9855111afa1
CrisCanonDev <criscanon21@gmail.com>
2024-04-18 08:48:29 +0300

    Firs Commit

---
+++ src/main/java/com/swegroup3/Sports/Club/App/SportsClubAppApplication.java
@@ -0,0 +1,13 @@
+package com.swegroup3.Sports.Club.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsClubAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsClubAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swegroup3/Sports/Club/App/SportsClubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swegroup3.Sports.Club.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsClubAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
