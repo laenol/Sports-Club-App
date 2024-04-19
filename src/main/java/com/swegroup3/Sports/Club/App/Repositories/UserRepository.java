108d31a3e6fe0d9c28ff424af96cc6e299bfb0c9
CrisCanonDev <criscanon21@gmail.com>
2024-04-19 17:06:38 +0300

    implement interface method to retrieve user by Username

--- src/main/java/com/swegroup3/Sports/Club/App/Repositories/UserRepository.java
+++ src/main/java/com/swegroup3/Sports/Club/App/Repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.swegroup3.Sports.Club.App.Repositories;
 import com.swegroup3.Sports.Club.App.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    User getUserByUsername(@Param("username") String username);
 }
