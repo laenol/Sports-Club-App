61edb7605c0d4afeee3c7bcf25637d4501eb6f5e
CrisCanonDev <criscanon21@gmail.com>
2024-04-19 17:53:08 +0300

    Command line running to adding values to database via console

--- src/main/java/com/swegroup3/Sports/Club/App/SportsClubAppApplication.java
+++ src/main/java/com/swegroup3/Sports/Club/App/SportsClubAppApplication.java
@@ -1,13 +1,38 @@
 package com.swegroup3.Sports.Club.App;
 
+import com.swegroup3.Sports.Club.App.Entities.User;
+import com.swegroup3.Sports.Club.App.Repositories.RoleRepository;
+import com.swegroup3.Sports.Club.App.Repositories.UserRepository;
+import com.swegroup3.Sports.Club.App.Services.UserService;
+import com.swegroup3.Sports.Club.App.security.SecuredPassword;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SportsClubAppApplication {
+public class SportsClubAppApplication{
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportsClubAppApplication.class, args);
 	}
 
+
+	//Checking database implementation via CommandLine ("Testing ORM")
+
+//	@Autowired
+//	private UserService userService;
+//	@Autowired
+//	private UserRepository userRepository;
+//
+//	@Autowired
+//	private RoleRepository roleRepository;
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		SecuredPassword encryptionPassword =  new SecuredPassword("password");
+//		System.out.println(encryptionPassword.getEncodePassword());
+//		userService.createUser(new User(1L, "Cristhian","cris21", encryptionPassword.getEncodePassword(), roleRepository.getReferenceById(1L)));
+//	}
 }
