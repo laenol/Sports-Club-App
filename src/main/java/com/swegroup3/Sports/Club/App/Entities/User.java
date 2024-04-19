3bbe57e80106c0bb252b516d1d66236a4361fe2b
CrisCanonDev <criscanon21@gmail.com>
2024-04-19 16:31:52 +0300

    renaming entity

--- src/main/java/com/swegroup3/Sports/Club/App/Entities/User.java
+++ src/main/java/com/swegroup3/Sports/Club/App/Entities/User.java
@@ -3,16 +3,14 @@ package com.swegroup3.Sports.Club.App.Entities;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Generated;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 
 @Entity
 @Data
 @Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Users {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/swegroup3/Sports/Club/App/Repositories/UserRepository.java
+++ src/main/java/com/swegroup3/Sports/Club/App/Repositories/UserRepository.java
@@ -1,10 +1,9 @@
 package com.swegroup3.Sports.Club.App.Repositories;
-
-import org.apache.catalina.User;
+import com.swegroup3.Sports.Club.App.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
