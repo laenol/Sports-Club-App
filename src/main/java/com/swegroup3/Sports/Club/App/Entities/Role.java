13771065c3828b26dee141bb72d25157aaec17e2
CrisCanonDev <criscanon21@gmail.com>
2024-04-19 17:04:14 +0300

    Role entity and repository

---
+++ src/main/java/com/swegroup3/Sports/Club/App/Entities/Role.java
@@ -0,0 +1,19 @@
+package com.swegroup3.Sports.Club.App.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/swegroup3/Sports/Club/App/Repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.swegroup3.Sports.Club.App.Repositories;
+
+import com.swegroup3.Sports.Club.App.Entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
