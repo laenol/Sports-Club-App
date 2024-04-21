d1880773d9af51538684ab6a8abfcb673cee0546
CrisCanonDev <criscanon21@gmail.com>
2024-04-21 11:33:23 +0300

    Team entity creation

---
+++ src/main/java/com/swegroup3/Sports/Club/App/Entities/Team.java
@@ -0,0 +1,24 @@
+package com.swegroup3.Sports.Club.App.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "teams")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "team_id")
+    private List<User> members;
+}
