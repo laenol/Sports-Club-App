fb951967da460c3a54781ab48f0ecc4c36644238
CrisCanonDev <criscanon21@gmail.com>
2024-04-21 12:08:37 +0300

    Team repository findName method

--- src/main/java/com/swegroup3/Sports/Club/App/Repositories/TeamRepository.java
+++ src/main/java/com/swegroup3/Sports/Club/App/Repositories/TeamRepository.java
@@ -4,6 +4,9 @@ import com.swegroup3.Sports.Club.App.Entities.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    Optional<Team> findByName(String name);
 }
