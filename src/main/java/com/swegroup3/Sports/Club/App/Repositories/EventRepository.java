dc9b3f08b16031a9de57ca4b133c19a430fe8cd6
CrisCanonDev <criscanon21@gmail.com>
2024-04-23 14:52:16 +0300

    amending event repository

--- src/main/java/com/swegroup3/Sports/Club/App/Repositories/EventRepository.java
+++ src/main/java/com/swegroup3/Sports/Club/App/Repositories/EventRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends JpaRepository<Long, Event> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 }
