f775b3870f75a0e8d31beed4bfecb0cfc00edc3a
CrisCanonDev <criscanon21@gmail.com>
2024-04-23 14:52:48 +0300

    Event service and its implementations

---
+++ src/main/java/com/swegroup3/Sports/Club/App/Services/EventService.java
@@ -0,0 +1,21 @@
+package com.swegroup3.Sports.Club.App.Services;
+
+import com.swegroup3.Sports.Club.App.Entities.Event;
+import com.swegroup3.Sports.Club.App.Entities.Team;
+import com.swegroup3.Sports.Club.App.Entities.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface EventService {
+    Event SaveEvent(Event event);
+    Optional<Event> findById(Long id);
+    List<Event> listAllEvents();
+    Event updateEvent(Long id, Event event);
+
+    Event addUserToEvent(Long id,Event event, User user);
+    void deleteEvent(Long id);
+
+}
---
+++ src/main/java/com/swegroup3/Sports/Club/App/Services/implementations/EventServiceImpl.java
@@ -0,0 +1,64 @@
+package com.swegroup3.Sports.Club.App.Services.implementations;
+
+
+import com.swegroup3.Sports.Club.App.Entities.Event;
+import com.swegroup3.Sports.Club.App.Entities.Team;
+import com.swegroup3.Sports.Club.App.Entities.User;
+import com.swegroup3.Sports.Club.App.Repositories.EventRepository;
+import com.swegroup3.Sports.Club.App.Services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class EventServiceImpl implements EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+    @Override
+    public Event SaveEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    @Override
+    public Optional<Event> findById(Long id) {
+        return eventRepository.findById(id);
+    }
+
+    @Override
+    public List<Event> listAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    @Override
+    public Event updateEvent(Long id, Event event) {
+        Event eventDDBB = eventRepository.findById(id).orElse(null);
+        if(eventDDBB != null){
+            eventDDBB.setName(event.getName());
+            eventDDBB.setTeam(event.getTeam());
+            eventDDBB.setDescription(event.getDescription());
+            eventDDBB.setUsers(event.getUsers());
+            eventDDBB.setMax_amount_participants(event.getMax_amount_participants());
+            eventDDBB.setLocation(event.getLocation());
+        }
+        return event ;
+    }
+
+    @Override
+    public Event addUserToEvent(Long id, Event event, User user){
+        Event eventDDBB = eventRepository.findById(id).orElse(null);
+        if(eventDDBB != null){
+            Set<User> tempUsers = event.getUsers();
+            tempUsers.add(user);
+            eventDDBB.setUsers(tempUsers);
+        }
+        return event;
+    }
+    @Override
+    public void deleteEvent(Long id) {
+        eventRepository.deleteById(id);
+    }
+}
